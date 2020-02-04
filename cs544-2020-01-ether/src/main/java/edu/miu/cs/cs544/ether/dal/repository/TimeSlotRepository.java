4f02719ff5d6bce674e90808d9618154f37f60c1
emito <aigemito.2006@gmail.com>
2020-02-04 17:48:15 -0600

    TimeSlot CRUD operation is Done

--- cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/dal/repository/TimeSlotRepository.java
+++ cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/dal/repository/TimeSlotRepository.java
@@ -4,6 +4,13 @@ import edu.miu.cs.cs544.ether.dal.entitiy.TimeSlot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TimeSlotRepository extends JpaRepository<TimeSlot, String> {
+    //	  Optional<TimeSlot> findTimeSlotById(long TimeSlotId);
+//	    Optional<TimeSlot> deleteTimeSlotById(long TimeSlotId);
+//		void delete(String name);
+    Optional<TimeSlot> findById(String abbreviation);
 }
--- cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/restcontroller/TimeSlotController.java
+++ cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/restcontroller/TimeSlotController.java
@@ -5,6 +5,10 @@ import edu.miu.cs.cs544.ether.dal.entitiy.TimeSlot;
 import java.util.List;
 
 public interface TimeSlotController {
-    List<TimeSlot> getTimeSlot();
 
+    List<TimeSlot> getAll() throws Exception;
+    public TimeSlot getById(String abbreviation);
+    public TimeSlot create(TimeSlot timeSlot);
+    public TimeSlot update(TimeSlot timeSlot,String abbreviation);
+    public void delete(String abbreviation);
 }
