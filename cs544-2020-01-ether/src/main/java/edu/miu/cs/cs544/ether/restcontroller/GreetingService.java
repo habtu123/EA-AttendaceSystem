1d4cd1c5fbf4354bf1d31c68db293de4cae8d516
emito <aigemito.2006@gmail.com>
2020-02-04 10:23:13 -0600

    all fortune and greeting class are deleted and student custom exception is added

--- cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/restcontroller/GreetingService.java
+++
@@ -1,7 +0,0 @@
-package edu.miu.cs.cs544.ether.restcontroller;
-
-import edu.miu.cs.cs544.ether.dal.Greeting;
-
-public interface GreetingService {
-    public Greeting getFortune();
-}
--- cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/service/impl/StudentServiceImpl.java
+++ cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/service/impl/StudentServiceImpl.java
@@ -7,17 +7,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 @Service
 public class StudentServiceImpl implements StudentService {
     @Autowired
     private StudentRepository studentRepository;
+
     @Override
     public List<Student> listOfStudent()  throws Exception {
-
         List<Student> students = studentRepository.findAll();
         if(students == null)
-            throw new Exception("no Students foudn!!");
+            throw new Exception("No Students found!!");
         return students;
+    }
+
+    @Override
+    public Student getStudent(Predicate<Student> predicate) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Student getStudent(Long Id) throws Exception {
+        Student student=studentRepository.findById(Id).get();
+        if(student==null){
+
+        }
+        return null;
     }
 }
