c7e267f40acc2194d92659a8959a4b9d6d29fcbb
ab3656 <ameetbht@gmail.com>
2020-02-05 10:35:56 -0600

    Added test case

--- cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/dal/entity/Course.java
+++ cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/dal/entity/Course.java
@@ -7,16 +7,50 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Entity
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String courseId;
     private String name;
-    @Lob
+    
+    public Course() {
+    	
+    }
+    
+    public Course(String courseId, String name, String description) {
+		this.courseId = courseId;
+		this.name = name;
+		this.description = description;
+	}
+    
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Lob
     private String description;
 
 }
--- cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/dal/entity/User.java
+++ cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/dal/entity/User.java
@@ -7,9 +7,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.util.List;
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-public @Data class User {
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -17,4 +15,39 @@ public @Data class User {
     private String password;
     @OneToMany()
     private List<UserRole> roles;
+    
+	public User(String username, String password, List<UserRole> roles) {
+		this.username = username;
+		this.password = password;
+		this.roles = roles;
+	}
+	
+	public User() {
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<UserRole> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<UserRole> roles) {
+		this.roles = roles;
+	}   
+    
 }
