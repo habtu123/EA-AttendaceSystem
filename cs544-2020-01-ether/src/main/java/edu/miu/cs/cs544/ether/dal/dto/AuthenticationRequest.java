ba108b5d736aa779486bc10b78a99e09afef7e38
habtu <habtassefa@gmail.com>
2020-02-06 10:49:45 -0600

    circut breaker and password encoder

--- cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/dal/dto/AuthenticationRequest.java
+++ cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/dal/dto/AuthenticationRequest.java
@@ -10,4 +10,6 @@ import lombok.NoArgsConstructor;
 public class AuthenticationRequest {
     private String username;
     private String password;
+
+
 }
--- cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/restcontroller/auth/AutheticationContoller.java
+++ cs544-2020-01-ether/src/main/java/edu/miu/cs/cs544/ether/restcontroller/auth/AutheticationContoller.java
@@ -1,11 +1,14 @@
 package edu.miu.cs.cs544.ether.restcontroller.auth;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import edu.miu.cs.cs544.ether.dal.dto.AuthenticationRequest;
 import edu.miu.cs.cs544.ether.dal.dto.AutheticationResponse;
 import edu.miu.cs.cs544.ether.security.MyUserDetailService;
 import edu.miu.cs.cs544.ether.security.jwt.JWTUtils;
 import edu.miu.cs.cs544.ether.security.jwt.JwtTokenUtilService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,15 +35,22 @@ public class AutheticationContoller {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${auth-url}")
+    private String authUrl;
     @PostMapping(value = "/autheticate")
+    @HystrixCommand(fallbackMethod = "defaultToken")
     public String createAutheticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception{
 
-        ResponseEntity<AutheticationResponse> jwt = restTemplate.postForEntity("http://auth/autheticate", authenticationRequest, AutheticationResponse.class);
+        ResponseEntity<AutheticationResponse> jwt = restTemplate.postForEntity(authUrl, authenticationRequest, AutheticationResponse.class);
 
         final UserDetails userDetails = userDetailService.loadUserByUsername(authenticationRequest.getUsername());
         final String token = jwtTokenUtilService.generateToken(userDetails);
 
         return jwt.getBody().getJwt();
+    }
+
+    public  String defaultToken(AuthenticationRequest authenticationRequest){
+        return "Sorry, You token is not availeble now";
     }
 
 }
